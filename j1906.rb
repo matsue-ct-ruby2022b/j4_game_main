971aee5253d23bddbcaf1b3ed603fddf8bc7b5fc
rariruto <rariruto@icloud.com>
2022-12-11 15:43:16 +0900

    時間表示関数作成

--- j1906.rb
+++ j1906.rb
@@ -18,6 +18,32 @@ class Support_chara
     end
 end
 
+class Clock
+    attr_accessor :now_day,:now_time,:date,:wake_up,:deadline,:hour,:minute
+    def initialize
+        @now_day=0
+        @now_time=0
+        @date=7
+        @wake_up=540
+        @deadline=720
+        @hour=(wake_up*now_time)/60
+        @minute=(wake_up+now_time)%60
+    end
+end
+
+clock=Clock.new
+
+class Story
+    attr_accessor :scene_num,:page,:story_num
+    def initialize
+        @scene_num=0 #シナリオ番号
+        @page=0      #ページ番号
+        @story_num=0 #文章番号
+    end
+end
+
+story=Story.new
+
 tanaka=Support_chara.new("tanaka")
 suzuki=Support_chara.new("suzuki")
 
@@ -31,6 +57,7 @@ frame=Image.new(924,700,[100,0,0])
 @story=[
     #序章(王様視点)
     [
+    [
         "あいうえお",
         "あいうえお",
         "あいうえお",
@@ -42,8 +69,10 @@ frame=Image.new(924,700,[100,0,0])
         "かきくけこ",
         "かきくけこ",
     ],
+],
     #キャラ選択
     [
+    [
         "さしすせそ",
         "さしすせそ",
         "さしすせそ",
@@ -55,8 +84,10 @@ frame=Image.new(924,700,[100,0,0])
         "たちつてと",
         "たちつてと",
     ],
+],
     #会話1(tanaka)
     [
+    [
         "たちつてと",
         "たちつてと",
         "たちつてと",
@@ -68,8 +99,10 @@ frame=Image.new(924,700,[100,0,0])
         "たちつてと",
         "たちつてと",
     ],
+],
     #会話2(tanaka)
     [
+    [
         "たちつてと",
         "たちつてと",
         "たちつてと",
@@ -81,8 +114,10 @@ frame=Image.new(924,700,[100,0,0])
         "たちつてと",
         "たちつてと",
     ],
+],
     #会話1(suzuki)
     [
+    [
         "たちつてと",
         "たちつてと",
         "たちつてと",
@@ -94,8 +129,10 @@ frame=Image.new(924,700,[100,0,0])
         "たちつてと",
         "たちつてと",
     ],
+],
     #会話2(suzuki)
-     [
+    [
+    [
         "たちつてと",
         "たちつてと",
         "たちつてと",
@@ -107,16 +144,47 @@ frame=Image.new(924,700,[100,0,0])
         "たちつてと",
         "たちつてと",
     ],
+],
+    #会話1(魔王戦闘前)
+    [
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+],
+    #会話2(魔王戦闘後)
+    [
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+],
 ]
 
 #シナリオ
-def scene(turn,num)
+def scene(scene,turn,num)
    
-    @story[turn][0..num].each_with_index do |n,i|
+    @story[scene][turn][0..num].each_with_index do |n,i|
        message("#{n}",50,50+i*(SIZE*2),@font)    
     end
 
-    if @story[turn][num]==nil
+    if @story[scene][turn][num]==nil
         false
     else 
         true
@@ -136,13 +204,29 @@ end
 #行動選択
 def move
     message("今から何をしようか",50,50,@font)
-    judge
+    branch=judge
 end
 
 #1日の流れ
 def live
-
+    
 end
+
+def output_limit(clock)
+    now_prog(clock)
+    if clock.minute<10 
+        message("#{clock.hour}:0#{clock.minute}",10,10,@font)
+    else 
+        message("#{clock.hour}:#{clock.minute}",10,10,@font)
+    end
+
+    message("残り#{clock.date-clock.now_day}",500,10,@font)
+end
+
+def now_prog(clock)
+    clock.hour=(clock.wake_up+clock.now_time)/60
+    clock.minute=(clock.wake_up+clock.now_time)%60
+end    
 
 #メッセージ出力
 def message(mes,pos_x,pos_y,font)
@@ -150,19 +234,18 @@ def message(mes,pos_x,pos_y,font)
 end
 
 #main文
-story_num=0
-page=0
 Window.loop do
-       if !scene(page,story_num)
-            page+=1
-            story_num=0
-        else
-            if Input.key_push?(K_RETURN)
-                story_num+=1
-            elsif Input.key_push?(K_LEFT)
-                page-=1
-                story_num=0
-            end
+    if !scene(story.scene_num,story.page,story.story_num)
+        story.page+=1
+        story.story_num=0
+    else
+        if Input.key_push?(K_RETURN)
+            story.story_num+=1
+        elsif Input.key_push?(K_LEFT)
+            story.page-=1
+            story.story_num=0
         end
-        Window.draw_alpha(50,30, frame, 128)
+    end
+    output_limit(clock)
+    Window.draw_alpha(50,30, frame, 128)
 end
