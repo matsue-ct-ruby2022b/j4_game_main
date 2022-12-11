6b6662e85bcf57d29a5c66f6a761bfd1f06260e6
rariruto <rariruto@icloud.com>
2022-12-11 11:39:33 +0900

    ファイル作成

--- j1906.rb
+++ j1906.rb
@@ -1,2 +1,74 @@
-#j1906縲繧ｹ繝医�ｼ繝ｪ繝ｼ
 
+require 'dxruby'
+
+#ストーリー
+@font=Font.new(SIZE,fontname="MS 明朝")
+
+@story=[
+    [
+        "あいうえお",
+        "あいうえお",
+        "あいうえお",
+        "あいうえお",
+    ],
+    [
+        "かきくけこ",
+        "かきくけこ",
+        "かきくけこ",
+        "かきくけこ",
+    ],
+    [
+        "さしすせそ",
+        "さしすせそ",
+        "さしすせそ",
+        "さしすせそ",
+    ],
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+]
+
+
+def scene(turn,num)
+   
+    @story[turn][0..num].each_with_index do |n,i|
+       Window.draw_font(50,50+i*(SIZE*2),"#{n}",@font,z:5)    
+    end
+
+    if @story[turn][num]==nil
+        false
+    else 
+        true
+    end
+
+end
+
+def judge
+    Window.draw_font(50,50),"今から何をしようか",@font,z:5)    
+end
+
+def live
+
+end
+
+#main文
+story_num=0
+page=0
+Window.loop do
+       if !scene(page,story_num)
+            page+=1
+            sroty_num=0
+        else
+            if Input.key_push?(K_RETURN)
+                sroty_num+=1
+            elsif Input.key_push?(K_LEFT)
+                page-=1
+                sroty_num=0
+            end
+        end
+    end
+
+end
