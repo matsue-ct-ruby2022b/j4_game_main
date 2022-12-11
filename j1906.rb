39ec1de51d163f5d5b4134fbae0b4e34f147b66d
rariruto <rariruto@icloud.com>
2022-12-11 14:30:52 +0900

    やることを定めた

--- j1906.rb
+++ j1906.rb
@@ -9,6 +9,18 @@ Window.height = 768
 TITLE=50
 SIZE=20
 
+class Support_chara
+    attr_accessor :name, :party, :love
+    def initialize(key)
+        @name=key  #キャラクター名
+        @party=false    #パーティに入っているか(true:入っている,false:入っていない)
+        @love=0     #好感度
+    end
+end
+
+tanaka=Support_chara.new("tanaka")
+suzuki=Support_chara.new("suzuki")
+
 #枠
 frame=Image.new(924,700,[100,0,0])
 
@@ -17,6 +29,7 @@ frame=Image.new(924,700,[100,0,0])
 
 #シナリオ
 @story=[
+    #序章(王様視点)
     [
         "あいうえお",
         "あいうえお",
@@ -29,11 +42,64 @@ frame=Image.new(924,700,[100,0,0])
         "かきくけこ",
         "かきくけこ",
     ],
+    #キャラ選択
     [
         "さしすせそ",
         "さしすせそ",
         "さしすせそ",
         "さしすせそ",
+    ],
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    #会話1(tanaka)
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    #会話2(tanaka)
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    #会話1(suzuki)
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+    ],
+    #会話2(suzuki)
+     [
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
+        "たちつてと",
     ],
     [
         "たちつてと",
@@ -47,7 +113,7 @@ frame=Image.new(924,700,[100,0,0])
 def scene(turn,num)
    
     @story[turn][0..num].each_with_index do |n,i|
-       Window.draw_font(50,50+i*(SIZE*2),"#{n}",@font,z:5)    
+       message("#{n}",50,50+i*(SIZE*2),@font)    
     end
 
     if @story[turn][num]==nil
@@ -57,9 +123,20 @@ def scene(turn,num)
     end
 end
 
-#行動選択
+#キャラ選択
+def chara_choice
+
+end
+
+#選択肢
 def judge
-    Window.draw_font(50,50,"今から何をしようか",@font,z:5)    
+
+end
+
+#行動選択
+def move
+    message("今から何をしようか",50,50,@font)
+    judge
 end
 
 #1日の流れ
@@ -68,8 +145,8 @@ def live
 end
 
 #メッセージ出力
-def message(mes,pos_x,pos_y)
-    Window.draw_font(pos_x,pos_y,"#{mes}",@font,z:5)    
+def message(mes,pos_x,pos_y,font)
+    Window.draw_font(pos_x,pos_y,"#{mes}",font,z:5)    
 end
 
 #main文
@@ -88,5 +165,4 @@ Window.loop do
             end
         end
         Window.draw_alpha(50,30, frame, 128)
-
 end
