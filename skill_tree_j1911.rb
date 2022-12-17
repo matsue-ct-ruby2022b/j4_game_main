3f1fdfca0587f7d2bce9fbbb43368a9ebe7a7d1c
tasukukazaana <tasukukazaana@gmail.com>
2022-12-17 11:23:23 +0900

    nodeの選択機能

--- skill_tree_j1911.rb
+++ skill_tree_j1911.rb
@@ -2,6 +2,8 @@ require 'dxruby'
 
 Window.width=1024
 Window.height=768
+Window.x = 0
+Window.y = 0
 
 NODE_MAX_NUM = 29
 ROOT_X = (Window.width-200)/2
@@ -131,42 +133,42 @@ class SkillTree
         node.release_flag = 1
     end
 
-    def detail_print(node) #選択されたノードを渡す
+    def detail_print(n) #選択されたノードを渡す
         detail_font = Font.new(20)
         ui_font = Font.new(32)
-        coin_img = Image.load("images/money.png")
-
-        
+        node_release_button = Image.load("images/node_release_button.png")
+        node = get_node(n)
             x = Input.mouse_pos_x  # マウスカーソルのx座標
             y = Input.mouse_pos_y  # マウスカーソルのy座標
             detail_pos_y = 50 #説明文の1行目のy座標
-            #Window.draw_font(100, 100, "x : #{x}, y : #{y}", detail_font)
-            Window.draw_box_fill(800, 0, 1023, 767, C_WHITE, z=1)
+            Window.draw_box_fill(801, 0, 1023, 767, C_WHITE, z=1) #220x767
             Window.draw_font(870, 700, "#{node.effected_status}", ui_font, color:C_BLACK, z:2)
 
-            #debug用変数,Nodeクラスのメンバを用いる
-            str1 = " HPが10増加します"
-            str2 = " -500G"
-            node_name = "HP+++"
             Window.draw_font(850, detail_pos_y, "「#{node.effected_status}」", Font.new(22), color:C_BLACK, z:2)
             Window.draw_font(810, detail_pos_y + 30, "効果:", detail_font, color:C_BLACK, z:2)
-            Window.draw_font(810, detail_pos_y + 50, "#{str1}", detail_font, color:C_BLACK, z:2)
+            Window.draw_font(810, detail_pos_y + 50, "#{node.effected_status}が#{node.effect_value}増加します", detail_font, color:C_BLACK, z:2)
             Window.draw_font(810, detail_pos_y + 80, "金額:", detail_font, color:C_BLACK, z:2)
-            Window.draw_font(810, detail_pos_y + 100, "#{str2}", detail_font, color:C_BLACK, z:2)
+            Window.draw_font(810, detail_pos_y + 100, "-#{node.need_money}G", detail_font, color:C_BLACK, z:2)
+            if node.release_flag == 1
+                Window.draw_font(870, detail_pos_y + 150, "開放済み", detail_font, color:C_RED, z:2)
+            else
+                Window.draw(911-node_release_button.width/2, 620, node_release_button, z=2) #開放ボタンの表示
+            end
             #Window.draw_scale(890, 120, coin_img, 0.1, 0.1, z=5)
 
-            if 860 <= x && x <= 950 && 640 <= y && y <= 680
+            if 830 <= x && x <= 990 && 620 <= y && y <= 700
                 #コマンド選択画像
-                Window.draw_box(860, 690, 950, 740, C_BLACK, z=4)
+                Window.draw_box(911-node_release_button.width/2 - 5, 615, 911 + node_release_button.width/2 + 5, 615 + node_release_button.height + 5, C_BLACK, z=2)
+
                 if Input.mousePush?(M_LBUTTON)
                     #強化処理
-                    #break
+                    node.release_flag = 1
                 end
             end
     end
 
     #マウスクリックされたら呼ばれる, ノード番号を返す
-    def click_node(x,y)
+    def click_node(x,y,node) #nodeは前にクリックされたノードを入れる
         x = Input.mouse_pos_x  # マウスカーソルのx座標
         y = Input.mouse_pos_y  # マウスカーソルのy座標
 
@@ -178,7 +180,7 @@ class SkillTree
                 end
             end
         end
-        return nil
+        return node #node以外をクリックした時は前にクリックされたノードを返す
     end
 
     def debug_print
@@ -197,14 +199,14 @@ tree.show(tree.root)
 #tree.open_node(3)
 #node=tree.get_node(3)
 #puts "#{node.node_num},#{node.x},#{node.y},#{node.release_flag}"
-selected_node = NODE_MAX_NUM + 1 #初期化, nodeの最大値+1を持たせる
+selected_node = 0 #初期化, rootにする
 Window.loop do
     x=Input.mouse_pos_x
     y=Input.mouse_pos_y
 
     #表示関連
     Window.draw_box_fill(800, 0, 1023, 767, C_WHITE, z=1)
-    Window.draw_font(850, 100, "「#{x},#{y}」", Font.new(22), color:C_BLACK, z:2)
+    #Window.draw_font(850, 100, "「#{x},#{y}」", Font.new(22), color:C_BLACK, z:2) #カーソルの座標表示
     NODE_MAX_NUM.times do |i|
         node=tree.get_node(i)
         node.children.each do |child|
@@ -220,10 +222,11 @@ Window.loop do
     end
 
     if Input.mouse_push?(M_LBUTTON)
-        selected_node = tree.click_node(x,y)
+        selected_node = tree.click_node(x,y,selected_node)
     end
-    if(selected_node != NODE_MAX_NUM + 1) #selected_nodeの初期化時の数値以外だったた()
-        Window.draw_font(870, 50, "#{selected_node}", Font.new(32), color:C_BLACK, z:2)
+    if selected_node != nil #selected_nodeがnil以外の時
+        #Window.draw_font(870, 50, "#{selected_node}", Font.new(32), color:C_BLACK, z:2) #debug用
+        tree.detail_print(selected_node)
     end
 
 end
